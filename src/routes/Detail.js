d4301c40c0f21457f4d48e35635aac85e072ab55
Seung <omg7152@naver.com>
2023-04-11 14:46:56 +0900

    all commit

---
+++ src/routes/Detail.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function Detail() {
+  const { id } = useParams();
+  const [info, setInfo] = useState({});
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setInfo(json.data.movie);
+  };
+
+  useEffect(() => {
+    getMovie();
+  });
+
+  return (
+    <div>
+      <img src={info.large_cover_image}></img>
+      <h1>{info.title}</h1>
+      <p>{info.description_full}</p>
+      <p>평점 : {info.rating}</p>
+    </div>
+  );
+}
+export default Detail;
